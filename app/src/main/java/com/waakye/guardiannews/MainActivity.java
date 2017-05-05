849ba139abaa2c7f27ea1f909de6a335171d662e
waakye <dailyomnivore@gmail.com>
2017-05-05 17:24:42 +0800

    Add a layout and code to MainActivity that creates a list of fake news articles

--- app/src/main/java/com/waakye/guardiannews/MainActivity.java
+++ app/src/main/java/com/waakye/guardiannews/MainActivity.java
@@ -1,7 +1,11 @@
 package com.waakye.guardiannews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a fake list of news article results
+        ArrayList<String> newsArticles = new ArrayList<String>();
+        newsArticles.add("10 Ways to Hike a Mountain");
+        newsArticles.add("10 Ways to Swim the English Channel");
+        newsArticles.add("10 Ways to Reduce Your Marathon Time");
+        newsArticles.add("10 Ways to Play Golf");
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsArticlesListView = (ListView) findViewById(R.id.news_listview);
+
+        // Create a new {@link ArrayAdapter} of news articles
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, newsArticles);
+
+        // Set the adapter on the {@link ListView} so that the list can be populated in the
+        // user interface
+        newsArticlesListView.setAdapter(adapter);
     }
 }
