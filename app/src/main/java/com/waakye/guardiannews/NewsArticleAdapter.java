70c9ad9c848cf67e034ef3b8e1dc663c04948998
waakye <dailyomnivore@gmail.com>
2017-05-07 02:37:45 +0800

    Format date of news article

--- app/src/main/java/com/waakye/guardiannews/NewsArticleAdapter.java
+++ app/src/main/java/com/waakye/guardiannews/NewsArticleAdapter.java
@@ -15,6 +15,10 @@ import java.util.List;
 
 public class NewsArticleAdapter extends ArrayAdapter<NewsArticle> {
 
+    private static final String DATE_SEPARATOR = "T";
+
+    private static final String HYPHEN_SEPARATOR = "-";
+
     /**
      * Constructs a new {@link NewsArticleAdapter}
      *
@@ -48,10 +52,81 @@ public class NewsArticleAdapter extends ArrayAdapter<NewsArticle> {
         // Display the section of the current article in that TextView
         articleSectionTextView.setText(currentNewsArticle.getSectionName());
 
+        // Get the original web publication date string from the NewsArticle object
+        String originalDate = currentNewsArticle.getWebPublicationDate();
+
+        // If the original location string contains "T" then store the date separately from the time
+        String primaryDate = null;
+        String secondaryDate = null;
+        String year = null;
+        String month = null;
+        String day = null;
+        int integerMonth = 0;
+
+        // Check whether the originalDate string contains the "T" text
+        if (originalDate.contains(DATE_SEPARATOR)) {
+            // Split the string into different parts (as an array of Strings) based on the "T" text.
+            // We expect an array of 2 Strings, where the first String will be a date and the
+            // second String will be a time
+            String[] parts = originalDate.split(DATE_SEPARATOR);
+            // Time should be
+            secondaryDate = parts[0];
+
+            String[] dateParts = secondaryDate.split(HYPHEN_SEPARATOR);
+            year = dateParts[0];
+            month = dateParts[1];
+            day = dateParts[2];
+            integerMonth = Integer.parseInt(month);
+
+            switch (integerMonth) {
+                case 1:
+                    month = "January";
+                    break;
+                case 2:
+                    month = "February";
+                    break;
+                case 3:
+                    month = "March";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "May";
+                    break;
+                case 6:
+                    month = "June";
+                    break;
+                case 7:
+                    month = "July";
+                    break;
+                case 8:
+                    month = "August";
+                    break;
+                case 9:
+                    month = "September";
+                    break;
+                case 10:
+                    month = "October";
+                    break;
+                case 11:
+                    month = "November";
+                    break;
+                case 12:
+                    month = "December";
+                    break;
+            }
+            secondaryDate = month + " " + day + ", " + year;
+        } else {
+            // Otherwise
+            secondaryDate = originalDate;
+        }
+
+
         // Find the TextView with the view ID publication_date
         TextView publicationDateTextView = (TextView)listItemView.findViewById(R.id.pubication_date);
         // Display the date of the current article in that TextView
-        publicationDateTextView.setText(currentNewsArticle.getWebPublicationDate());
+        publicationDateTextView.setText(secondaryDate);
 
         // Return the list item view that now shows the appropriate data
         return listItemView;
